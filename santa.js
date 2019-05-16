1962f04be978970032bc3c478e4334e6fd0e249e
Michael Guillemette <mike.guillemette@gmail.com>
2019-05-16 12:19:32 -0400

    Seems to be working in some way, but not getting what the tests expect as far as calls being made

--- santa.js
+++ santa.js
@@ -1,3 +1,15 @@
 export function deliverPresents(houses) {
-  console.log('Delivering presents to ' + houses[0])
+  if (houses.length === 1) {
+    console.log('Delivering presents to ' + houses[0])
+    return 0
+  }
+
+  var half = Math.ceil(houses.length / 2)
+  var firstHouses = houses.splice(0, half)
+  
+  // console.log(half)
+  // console.log(firstHouses)
+  
+  deliverPresents(firstHouses)
+  deliverPresents(houses)
 }
